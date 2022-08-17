6ad2148ffc2a6e923232a440b5bc699b4eb9e258
Semih Gencturk <semihgencturk@gmail.com>
2022-08-17 20:09:08 +0300

    add something

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Student from "./pages/Student";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Iaeste-Ogrenci" element={<Student />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Student.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Student = () => {
+  return <div>Bu sayfa hazırlanmaktadır.</div>;
+};
+
+export default Student;
